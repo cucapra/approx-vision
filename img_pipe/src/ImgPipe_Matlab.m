572a263c8fa7c40e404801fb18a24d5b848147ff
mbuckler <markabuckler@gmail.com>
2016-06-16 22:02:17 -0400

    Matlab image pipeline with image import, model import, demosaic, and first attempt at color space

--- img_pipe/src/ImgPipe_Matlab.m
+++ img_pipe/src/ImgPipe_Matlab.m
@@ -16,6 +16,15 @@
 % 
 %==============================================================
 
+% Model directory
+model_dir     = '../camera_models/NikonD40/Fl(L14)/';
+
+% Image directory
+image_dir     = '../imgs/';
+
+% Input image
+in_image_name = 'flower.NEF.raw_1C.tiff';
+
 %==============================================================
 % Import Forward Model Data
 %
@@ -23,9 +32,6 @@
 % camera setting and transform. This is not the case for 
 % every folder, but it is for the Nikon D40 on the Normal
 % setting and with Fl(L14)/florescent color.
-
-% Model directory
-model_dir = '../camera_models/NikonD40/Fl(L14)/';
 
 % Model file reading
 transforms_file  = table2array( readtable( ...
@@ -48,11 +54,12 @@ Ts             = transforms_file(1:3,:);
 Tw             = diag(transforms_file(8,:));
 
 % Combined transforms
+TsTw           = Ts*Tw;
 TsTw_file      = transforms_file(5:7,:);
 
 % Perform quick check to determine equivalence with provided model
 % Round to nearest 4 decimal representation for check
-TsTw_4dec      = round(Ts*Tw*10000)/10000;
+TsTw_4dec      = round(TsTw*10000)/10000;
 TsTw_file_4dec = round(TsTw_file*10000)/10000;
 assert( isequal( TsTw_4dec, TsTw_file_4dec), ...
     'Transform multiplication not equal to result found in model file' ) 
@@ -74,10 +81,44 @@ f              = resp_funct_file;
 % BLANK FOR NOW
 
 %==============================================================
-% Import Image Data
+% Import Raw Image Data
 
+% NOTE: Can use RAW2TIFF.cpp to convert raw to tiff. This isn't
+% automatically called by this script yet, but could be.
+
+in_image = imread(strcat(image_dir,in_image_name));
 
 %==============================================================
 % Forward pipeline function
 
-% 
+% Scale input image from 12 bit values to 16 bit values
+% (multiply by 2^4)
+in_scaled        = in_image * 16;
+
+% Convert to uint16 representation
+in_image_unit16  = im2uint16(in_scaled);
+
+% Demosaic image
+demosaiced       = demosaic(in_image_unit16,'gbrg');
+
+% Cast to float for rest of processing
+image_float      = im2double(demosaiced);
+
+% Apply color space transform and white balance transform
+
+% Better way to do this?
+transformed      = image_float;
+for y = 1:(size(image_float,1)/2)
+    for x = 1:size(image_float,2)
+        transformed(y,x,:) = transpose(squeeze(image_float(y,x,:))) * transpose(TsTw);
+    end
+end
+
+out_image = transformed;
+
+%==============================================================
+% Export Image
+
+
+
+imwrite(out_image,strcat(image_dir,in_image_name,'.output.tif'));
