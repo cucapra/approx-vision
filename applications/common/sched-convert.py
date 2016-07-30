860b9c29034cbbfb77df7327572abe7b1ce37f92
mbuckler <markabuckler@gmail.com>
2016-07-30 19:50:59 -0400

    Updated for corner case for multiple of number of threads

--- applications/common/sched-convert.py
+++ applications/common/sched-convert.py
@@ -6,10 +6,10 @@ from os import listdir
 from os.path import isfile, join
 
 # The directory to convert
-image_dir   = '/work/mark/datasets/coco-2014/val2014/'
+image_dir   = '/ufs/vlsi/mbuckler/scratch/'
 
 # Number of threads to use
-num_threads = 15
+num_threads = 4
 
 # Get list of files in directory
 file_list = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
@@ -24,22 +24,29 @@ subprocess.call(['mkdir',image_dir+'converted'])
 # Initialize bounds for running the pipe
 imgs_per_thread = num_imgs / num_threads
 start_img_id = 0
-end_img_id   = start_img_id + imgs_per_thread 
+end_img_id   = start_img_id + imgs_per_thread - 1
+
+print(num_imgs)
 
 # For every split
 for x in range(0,num_threads):
   # Provide a temporary folder for this script
   subprocess.call(['mkdir',image_dir+'temp'+str(x)])
   # In case the number of images is not a multiple of the number of threads
-  if (end_img_id > num_imgs):
-    end_img_id = num_imgs
+  if (num_imgs-start_img_id <= 2*imgs_per_thread):
+    end_img_id = num_imgs - 1
+
+  print(start_img_id)
+  print(end_img_id)
+
   # Start a script to process a subset of the images
-  pid = subprocess.Popen(["./run-pipe.py",
-                          str(x),
-                          str(start_img_id),
-                          str(end_img_id),
-                          image_dir
-                         ])
+#  pid = subprocess.Popen(["./run-pipe.py",
+#                          str(x),
+#                          str(start_img_id),
+#                          str(end_img_id),
+#                          image_dir
+#                         ])
+
   # Update the bounds for the next script
   start_img_id = start_img_id + imgs_per_thread
   end_img_id   = end_img_id   + imgs_per_thread
