0725b03069cce37ef8ac0e80f2dceb2776fb0ea8
mbuckler <markabuckler@gmail.com>
2016-08-07 13:08:09 -0400

    Updated analysis with smaller input size and more granular histogram bins

--- analysis/analyze_cifar10.py
+++ analysis/analyze_cifar10.py
@@ -7,42 +7,45 @@ import numpy as np
 # Only argument is path to cifar-10 data file
 filename = sys.argv[1]
 
-# Establish empty histogram
-histogram = np.zeros(16)
-
-# Initialize sum as zero
-total_sum = 0
+# Establish empty histograms
+histogram_r = np.zeros(256)
+histogram_g = np.zeros(256)
+histogram_b = np.zeros(256)
 
 with open(filename, 'rb') as f:
-  for i in range(0,10000):
+  for i in range(0,100): # 10000
     # Read in the image label
     byte_s = f.read(1)
-
-    # Initialize sum as zero
-    img_sum = 0
 
     for c in range(0,3):
       for y in range(0,32):
         for x in range(0,32):
           byte_s      = f.read(1)
-          channel_val = float(ord(byte_s[0]))
-          img_sum    += channel_val
-          hist_idx    = int(channel_val/16)
-          histogram[hist_idx] += 1
+          hist_idx    = ord(byte_s[0])
+          if c == 0:
+            histogram_r[hist_idx] += 1
+          if c == 1:
+            histogram_g[hist_idx] += 1
+          if c == 2:
+            histogram_b[hist_idx] += 1
 
-    img_avg = img_sum / float(3*32*32)
+# Normalize the histograms
+histogram_r = histogram_r / max(histogram_r)
+histogram_g = histogram_g / max(histogram_g)
+histogram_b = histogram_b / max(histogram_b)
 
-    total_sum = total_sum + img_avg
+plt.figure(1)
+plt.subplot(311)
+plt.bar(range(0,256),histogram_r,width=0.5,color='r')
+plt.axis([0,255,0,1])
 
-total_avg = total_sum / float(10000)
+plt.subplot(312)
+plt.bar(range(0,256),histogram_g,width=0.5,color='g')
+plt.axis([0,255,0,1])
 
-print('Total channel average:')
-print(total_avg)
-
-# Normalize the histogram
-histogram = histogram / max(histogram)
-
-plt.bar(range(16),histogram)
+plt.subplot(313)
+plt.bar(range(0,256),histogram_b,width=0.5,color='b')
+plt.axis([0,255,0,1])
 
 plt.show()
 
