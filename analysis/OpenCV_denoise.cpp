577e5a0a710eec273e662d373b7b949ee1f27648
Mark Buckler <markabuckler@gmail.com>
2016-09-29 18:16:41 -0400

    Adding renoising and denoising experiments

---
+++ analysis/OpenCV_denoise.cpp
@@ -0,0 +1,31 @@
+//
+// OpenCV Load 3 Channel Image Benchmark
+//
+// This file loads a three channel image and performs
+// denoising
+
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv; 
+
+int main(int argc, char** argv )
+{
+
+  Mat in_img;
+
+//  in_img = imread("../benchmark_images/beer_hall.raw_3C.png",CV_16UC3);
+  in_img = imread("../powersim/benchmark_images/beer_hall.raw_3C.png");
+
+  Mat in8bit(in_img.rows,in_img.cols,CV_8UC3);
+  Mat out_img(in_img.rows,in_img.cols,CV_8UC3);
+
+  in_img.convertTo(in8bit,CV_8UC3);
+
+  fastNlMeansDenoisingColored(in8bit,out_img);
+
+  //imwrite("out.png",out_img);
+
+  return 0;
+
+}
