8d7660069f969bfc097d9b0b326a9179aff0ea6a
Mark Buckler <markabuckler@gmail.com>
2016-10-14 02:56:22 -0400

    Update scheduler to work with new format

--- pipelines/cifar-10/cifar-sched-convert.py
+++ pipelines/cifar-10/cifar-sched-convert.py
@@ -13,29 +13,29 @@ if (len(sys.argv) < 2):
 version       = sys.argv[1]
 
 # Directory with virgin cifar data files
-data_in_dir   = '../data/cifar10/'
+data_in_dir   = '~/datasets/cifar-10/v0/'
 
 # Directory to put the converted files
-data_out_dir  = '../data/cifar10/converted-V'+str(version)+'/'
+data_out_dir  = '~/datasets/cifar-10/v'+str(version)+'/'
 
 # Data batch names
-data_names    = ['test_batch',
-  'data_batch_1',
-  'data_batch_2',
-  'data_batch_3',
-  'data_batch_4',
-  'data_batch_5']
+data_names    = ['test_batch.bin',
+  'data_batch_1.bin',
+  'data_batch_2.bin',
+  'data_batch_3.bin',
+  'data_batch_4.bin',
+  'data_batch_5.bin']
 
 # Create the directory for the converted files
 subprocess.call(["mkdir",data_out_dir])
 
 # Compile the converter
-subprocess.call(["make",('V'+str(version))])
+subprocess.call(["make",('version='+str(version))])
 
 # For each batch
 for x in range(0,6):
   # Start a script to process a batch of the images
-  pid = subprocess.Popen(["./convert-V"+str(version),
-                          data_in_dir+data_names[x]+".bin",
-                          data_out_dir+data_names[x]+"_converted_V"+str(version)+".bin",
+  pid = subprocess.Popen(["./pipeline_V"+str(version),
+                          data_in_dir+data_names[x],
+                          data_out_dir+data_names[x],
                          ])
