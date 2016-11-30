280bb250edc3ccc0015f21f9bb01e9dc0c8fdf53
Mark Buckler <markabuckler@gmail.com>
2016-11-29 20:15:42 -0500

    Updated to be functional

--- pipelines/lfw/lfw-sched.py
+++ pipelines/lfw/lfw-sched.py
@@ -2,23 +2,28 @@
 
 import subprocess
 import sys
+from subprocess import call
 from os import listdir
 from os.path import isfile, join
 import psutil
-
+import time
 
 num_threads = 2
 datasetpath = '/datasets/lfw/'
-vers_to_run = [ 1]
-in_vers     = [ 0]
+
+vers_to_run = [ 1, 2]
+in_vers     = [ 0, 0]
 
 for i,version in enumerate(vers_to_run):
+
+  # Compile the new version to run
+  call('make --directory ../common/ version='+str(version),shell=True)
 
   inputpath   = datasetpath + 'v' + str(in_vers[i]) + '/'
   outputpath  = datasetpath + 'v' + str(version)+     '/'
 
   # Make the directory for this section  
-  subprocess.call(['mkdir',out_img_dir])
+  subprocess.call(['mkdir',outputpath])
 
   # Make a list of directories
   dir_list = [f for f in listdir(inputpath)]
@@ -28,42 +33,43 @@ for i,version in enumerate(vers_to_run):
   num_dirs = len(dir_list)
 
   # Initialize bounds for running
-  dirs_per_thread = num_dirs / num_dirs
+  dirs_per_thread = num_dirs / num_threads
   start_dir_id    = 0
-  end_dir_id      = start_dir_id + dirs_per+thread - 1
+  end_dir_id      = start_dir_id + dirs_per_thread - 1
 
   # Establish empty set of processes
   procs = []
 
-  # For every thread
+  # Spawn all threads
   for x in range(0,num_threads):
     # In case the number of images is not a multiple of the number of threads
-    if (num_imgs-start_img_id <= 2*imgs_per_thread):
-      end_img_id = num_imgs - 1
+    if (num_dirs-start_dir_id <= 2*dirs_per_thread):
+      end_dir_id = num_dirs - 1
 
     # Start a script to process a batch of the images
-    procs.append( subprocess.Popen(["python thread_sched.py",
-				    str(version),
-				    str(start_dir_id),
-				    str(end_dir_id),
-				    inputpath,
-				    outputpath,
-				    str(x)
-                                   ]) )
+    procs.append( subprocess.Popen("python thread-sched.py "+
+            str(version)+' '+
+            str(start_dir_id)+' '+
+            str(end_dir_id)+' '+
+            inputpath+' '+
+            outputpath+' '+
+            str(x)+' '
+                                   ,shell=True) )
 
     # Update the bounds for the next script
-    start_img_id = start_img_id + imgs_per_thread
-    end_img_id   = end_img_id   + imgs_per_thread
+    start_dir_id = start_dir_id + dirs_per_thread
+    end_dir_id   = end_dir_id   + dirs_per_thread
 
-    # Initialize the number of procs
-    num_busy_procs = num_threads
+  # Initialize the number of busy procs
+  num_busy_procs = num_threads
 
-    # Busy wait
-    while(num_busy_procs != 0):
-      ls = [psutil.Process(proc.pid) for proc in procs]
+  # Check every minute to see if all threads have finished
+  while(num_busy_procs != 0):
+    ls = [psutil.Process(proc.pid) for proc in procs]
 
+    gone, alive = psutil.wait_procs(ls, timeout=3)
 
+    num_busy_procs = len(alive)
 
-
-
+    time.sleep(5)
 
