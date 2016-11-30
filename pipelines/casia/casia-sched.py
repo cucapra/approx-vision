2c3103393b87a46f619df3b375387e1ac2afa26e
Mark Buckler <markabuckler@gmail.com>
2016-11-29 21:22:55 -0500

    Adding casia conversion scheduler

---
+++ pipelines/casia/casia-sched.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+import subprocess
+import sys
+from subprocess import call
+from os import listdir
+from os.path import isfile, join
+import psutil
+import time
+
+num_threads = 2
+datasetpath = '/datasets/casia/'
+
+vers_to_run = [ 1, 2]
+in_vers     = [ 0, 0]
+
+for i,version in enumerate(vers_to_run):
+
+  # Compile the new version to run
+  call('make --directory ../common/ version='+str(version),shell=True)
+
+  inputpath   = datasetpath + 'v' + str(in_vers[i]) + '/'
+  outputpath  = datasetpath + 'v' + str(version)+     '/'
+
+  # Make the directory for this section  
+  subprocess.call(['mkdir',outputpath])
+
+  # Make a list of directories
+  dir_list = [f for f in listdir(inputpath)]
+  dir_list.sort()
+ 
+  # Get number of directories
+  num_dirs = len(dir_list)
+
+  # Initialize bounds for running
+  dirs_per_thread = num_dirs / num_threads
+  start_dir_id    = 0
+  end_dir_id      = start_dir_id + dirs_per_thread - 1
+
+  # Establish empty set of processes
+  procs = []
+
+  # Spawn all threads
+  for x in range(0,num_threads):
+    # In case the number of images is not a multiple of the number of threads
+    if (num_dirs-start_dir_id <= 2*dirs_per_thread):
+      end_dir_id = num_dirs - 1
+
+    # Start a script to process a batch of the images
+    procs.append( subprocess.Popen("python thread-sched.py "+
+            str(version)+' '+
+            str(start_dir_id)+' '+
+            str(end_dir_id)+' '+
+            inputpath+' '+
+            outputpath+' '+
+            str(x)+' '
+                                   ,shell=True) )
+
+    # Update the bounds for the next script
+    start_dir_id = start_dir_id + dirs_per_thread
+    end_dir_id   = end_dir_id   + dirs_per_thread
+
+  # Initialize the number of busy procs
+  num_busy_procs = num_threads
+
+  # Check every minute to see if all threads have finished
+  while(num_busy_procs != 0):
+    ls = [psutil.Process(proc.pid) for proc in procs]
+
+    gone, alive = psutil.wait_procs(ls, timeout=3)
+
+    num_busy_procs = len(alive)
+
+    time.sleep(5)
+
---
+++ pipelines/casia/thread-sched.py
@@ -0,0 +1,51 @@
+from PIL import Image
+import subprocess
+from subprocess import call
+import sys
+from os import listdir
+from os.path import isfile, join
+
+# Receive command line arguments
+version      =     sys.argv[1]
+start_dir_id = int(sys.argv[2])
+end_dir_id   = int(sys.argv[3])
+in_dir_dir   =     sys.argv[4]
+out_dir_dir  =     sys.argv[5]
+thread_id    = int(sys.argv[6])
+
+# Make a list of directories
+dir_list = [f for f in listdir(in_dir_dir)]
+dir_list.sort()
+
+# For each directory
+for x in range(start_dir_id,end_dir_id+1):
+
+  inputpath   = in_dir_dir  + dir_list[x] + '/'
+  outputpath  = out_dir_dir + dir_list[x] + '/'
+
+  call(['mkdir',outputpath])
+
+  # Get list of files in directory
+  file_list = [f for f in listdir(inputpath) if f.endswith('.png')
+    and isfile(join(inputpath, f))]
+  file_list.sort()
+
+  # Number of images in the directory
+  num_imgs = len(file_list)
+
+  # Provide a temporary folder for this script
+  subprocess.call(['mkdir',inputpath+'temp'+str(thread_id)])
+  
+  # Process all images in this directory and wait till complete
+  pid = subprocess.call("../common/run-pipe.py"+" "+
+                            str(version)+" "+
+                            str(0)+" "+
+                            str(num_imgs-1)+" "+
+                            inputpath+" "+
+                            outputpath+" "+
+                            str(thread_id)
+                           ,shell=True)
+  
+  # Remove the temporary directory
+  subprocess.call('rm -rf '+inputpath+'temp'+str(thread_id),shell=True)
+
