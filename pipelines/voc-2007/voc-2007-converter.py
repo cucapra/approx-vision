45562d5c2cf7423dacb07904edd51746286ca2d6
Mark Buckler <markabuckler@gmail.com>
2016-11-29 20:48:40 -0500

    Updated to work with sequential computatiopn

--- pipelines/voc-2007/voc-2007-converter.py
+++ pipelines/voc-2007/voc-2007-converter.py
@@ -10,7 +10,7 @@ from os.path import isfile, join
 
 if len(sys.argv) < 3:
   print('Usage: \n ' +
-        'python voc-2012-converter <input_data_vers_number> <converter_version_number> <number_of_threads>')
+        'python voc-2007-converter <input_data_vers_number> <converter_version_number> <number_of_threads>')
   quit()
 
 in_version  = int(sys.argv[1])
@@ -51,6 +51,9 @@ imgs_per_thread = num_imgs / num_threads
 start_img_id = 0
 end_img_id   = start_img_id + imgs_per_thread - 1
 
+# Establish empty set of processes
+procs = []
+
 # For every split
 for x in range(0,num_threads):
   # Provide a temporary folder for this script
@@ -61,15 +64,29 @@ for x in range(0,num_threads):
     end_img_id = num_imgs - 1
 
   # Start a script to process a subset of the images
-  pid = subprocess.Popen(["../common/run-pipe.py",
+  procs.append( subprocess.Popen(["../common/run-pipe.py",
                             str(version),
                             str(start_img_id),
                             str(end_img_id),
                             in_img_dir,
                             out_img_dir,
                             str(x)
-                           ])
+                           ]) )
 
   # Update the bounds for the next script
   start_img_id = start_img_id + imgs_per_thread
   end_img_id   = end_img_id   + imgs_per_thread
+
+# Initialize the number of busy procs
+num_busy_procs = num_threads
+
+# Check every minute to see if all threads have finished
+while(num_busy_procs != 0):
+  ls = [psutil.Process(proc.pid) for proc in procs]
+
+  gone, alive = psutil.wait_procs(ls, timeout=3)
+
+  num_busy_procs = len(alive)
+
+  time.sleep(5)
+
