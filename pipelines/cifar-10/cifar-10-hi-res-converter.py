6a68c4cdccc3f081a7a54e70a62c1751f8d7483e
mbuckler <markabuckler@gmail.com>
2016-12-07 22:45:40 -0500

    Updated to include all image files, not just .jpeg

--- pipelines/cifar-10/cifar-10-hi-res-converter.py
+++ pipelines/cifar-10/cifar-10-hi-res-converter.py
@@ -6,7 +6,6 @@ import sys
 import os
 from os import listdir
 from os.path import isfile, join
-import psutil
 import time
 
 # Run alternative pipelines on the strecha-mvs dataset
@@ -20,7 +19,7 @@ in_version  = int(sys.argv[1])
 version     = int(sys.argv[2])
 num_threads = int(sys.argv[3])
 
-datasetpath = '/datasets/high-res-cifar-10/'
+datasetpath = '/work/mark/datasets/high-res-cifar-10/'
 inputpath   = datasetpath + 'v' + str(in_version) + '/'
 outputpath  = datasetpath + 'v' + str(version)+     '/'
 
@@ -28,8 +27,9 @@ call(['mkdir',outputpath])
 
 call('make --directory ../common/ version='+str(version),shell=True)
 
-classes = ['airplane','automobile','bird','cat','deer',
-           'dog','frog','horse','ship','truck']
+classes = ['airplane']
+#classes = ['airplane','automobile','bird','cat','deer',
+#           'dog','frog','horse','ship','truck']
    
 for class_ in classes:
 
@@ -39,9 +39,10 @@ for class_ in classes:
   call(['mkdir',out_img_dir])
 
   # Get list of files in directory
-  file_list = [f for f in listdir(in_img_dir) if f.endswith('.jpg')
-    and isfile(join(in_img_dir, f))]
+  file_list = [f for f in listdir(in_img_dir) 
+               if isfile(join(in_img_dir, f))]
   file_list.sort()
+
 
   # Number of images in the directory
   num_imgs = len(file_list)
