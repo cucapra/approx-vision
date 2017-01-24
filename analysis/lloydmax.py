1b98b8454b72317a93d52b9e5cb97fc19791f52d
mbuckler <markabuckler@gmail.com>
2017-01-24 17:37:13 -0500

    Successful lloydmax optimization

--- analysis/lloydmax.py
+++ analysis/lloydmax.py
@@ -150,8 +150,10 @@ def inverse_transform_sampling(data, n_bins=256, n_samples=1000):
         r = np.random.rand(n_samples)
         return inv_cdf(r)
 
-cdf_rand_samples = inverse_transform_sampling(pixel_data,256,2000)
+cdf_rand_samples = inverse_transform_sampling(pixel_data,256,100000)
 
+
+'''
 plt.figure(5)
 plt.subplot(2,1,1)
 plt.hist(pixel_data,256,normed=1)
@@ -160,6 +162,191 @@ plt.subplot(2,1,2)
 plt.hist(cdf_rand_samples,256,normed=1)
 plt.xlim([0,255])
 plt.show()
+'''
+
+###################################
+# Define functions for quantization experiments
+
+# First, we define a function which will calculate the RMSE
+def get_rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+# Convert rmse to psnr
+# Note that the maximum value used in the conversion is the 
+# maximum random sample, as error evaluations are made against
+# the original continuous samples
+def rmse2psnr(rmse):
+    return 20.0*np.math.log((255/rmse),10)
+
+# Here we define a linear quantizer with a configurable
+# number of bits
+def encode_linear(samples,num_bits):
+    # Largest sample in the data
+    max_sample     = max(samples)
+    # Largest possible value that can be used to encode
+    max_encode     = (2**num_bits)-1
+    # Scale samples so that their maximum is the largest
+    # possible encoding value
+    scaled_samples = [val*(max_encode/max_sample) for val in samples]
+    # Round to closest possible encoding value (integer)
+    encoded        = [round(val,0) for val in scaled_samples]
+    return max_sample, encoded
+
+# And here we have the accompanying decoder for the linear
+# quantizer
+def decode_linear(encoded,num_bits,max_sample):
+    # Largest possible value that could have been used to decode
+    max_encode     = (2**num_bits)-1
+    # Re-scale to decode
+    decoded   = [val * ( max_sample/max_encode) for val in encoded]
+    return decoded
+
+# Here we define a linear quantizer with a configurable
+# number of bits
+def encode_logarithmic(samples,num_bits):
+    # Convert the samples to the logarithmic space
+    samples = [np.log(val) for val in samples]
+    # Largest sample in the data
+    max_sample     = max(samples)
+    # Largest possible value that can be used to encode
+    max_encode     = (2**num_bits)-1
+    # Scale samples so that their maximum is the largest
+    # possible encoding value
+    scaled_samples = [val*(max_encode/max_sample) for val in samples]
+    # Round to closest possible encoding value (integer)
+    encoded        = [round(val,0) for val in scaled_samples]
+    return max_sample, encoded
+
+# And here we have the accompanying decoder for the logarithmic
+# quantizer
+def decode_logarithmic(encoded,num_bits,max_sample):
+    # Largest possible value that could have been used to decode
+    max_encode     = (2**num_bits)-1
+    # Re-scale to decode
+    decoded   = [val * ( max_sample/max_encode) for val in encoded]
+    # Finish by reversing the logarithmic encoding to linearize
+    decoded   = [np.exp(val) for val in decoded]
+    return decoded
+
+###################################
+# Linear quantization analysis
+
+# 12 bit linear
+max_sample, encoded_12bit = encode_linear(cdf_rand_samples,12)
+decoded_12bit = decode_linear(encoded_12bit,12,max_sample)
+rmse = get_rmse(decoded_12bit,cdf_rand_samples)
+print 'Linearly encoded raw 12 bit data'
+print '  RMSE: '+str(rmse)
+print '  PSNR: '+str(rmse2psnr(rmse))+' dB'
+
+# 8 bit linear
+max_sample, encoded_8bit = encode_linear(cdf_rand_samples,8)
+decoded_8bit = decode_linear(encoded_8bit,8,max_sample)
+rmse = get_rmse(decoded_8bit,cdf_rand_samples)
+print 'Linearly encoded raw 8 bit data'
+print '  RMSE: '+str(rmse)
+print '  PSNR: '+str(rmse2psnr(rmse))+' dB'
+
+###################################
+# Logarithmic quantization analysis
+
+###################################
+# Lloyd-max quantization analyis
+
+import scipy.integrate as integrate
+
+max_sample = max(cdf_rand_samples)
+
+'''
+def lognorm_func (in_):
+    return (np.exp(-(np.log(in_) - mu)**2 / (2 * sigma**2))
+        / (in_ * sigma * np.sqrt(2 * np.pi)))
+def scaled_lognorm_func (in_):
+    return in_ * (np.exp(-(np.log(in_) - mu)**2 / (2 * sigma**2))
+        / (in_ * sigma * np.sqrt(2 * np.pi)))
+'''
+
+def pdf_func (in_):
+    return PDF_cubic(in_)
+def scaled_pdf_func (in_):
+    return in_ * PDF_cubic(in_)
+
+
+# Represent with 8 bits
+num_bits = 8
+
+# The number of representation points
+M = (2**num_bits)
+
+# Initialize a and b
+lloydmax_a = [None] * M
+lloydmax_b = [None] * M
+init_step_size = max_sample/M
+for i in range(0,M):
+    lloydmax_b[i] = (i+1)*init_step_size
+for i in range(0,M):
+    lloydmax_a[i] = lloydmax_b[i] - (init_step_size/2)
+
+def lloydmax_encode_single (in_):
+    idx  = 0
+    while ( in_ > lloydmax_b[idx] ):
+        idx = idx + 1
+        if idx == M-1:
+            break
+    return idx
+
+def lloydmax_encode (in_):        
+    return [lloydmax_encode_single(val) for val in in_]
+    
+def lloydmax_decode (in_):
+    return [lloydmax_a[val] for val in in_]
+    
+encoded_lloydmax = lloydmax_encode(cdf_rand_samples)
+decoded_lloydmax = lloydmax_decode(encoded_lloydmax)
+rmse = get_rmse(decoded_lloydmax,cdf_rand_samples)
+print 'Test of Lloydmax initializer'
+print rmse
+    
+def lloydmax_learn (rmse_goal):
+    rmse = 1.0
+    while (rmse > rmse_goal):
+        # Re-initialize b
+        for idx in range(0,M-1):
+            lloydmax_b[idx] = 0.5*(lloydmax_a[idx+1]+lloydmax_a[idx])
+        # Re-initialize a
+        for idx in range(0,M):
+            if idx == 0:
+                integrate_low_end = 0
+            else:
+                integrate_low_end = lloydmax_b[idx-1]
+            integrate_high_end = lloydmax_b[idx]
+            
+            numerator,err   = integrate.quad(scaled_pdf_func,
+                                             integrate_low_end,
+                                             integrate_high_end)
+            denominator,err = integrate.quad(pdf_func,
+                                             integrate_low_end,
+                                             integrate_high_end)
+               
+            lloydmax_a[idx] = numerator/denominator
+        #print lloydmax_a[100]
+        # Test the current RMSE
+        encoded_lloydmax = lloydmax_encode(cdf_rand_samples)
+        decoded_lloydmax = lloydmax_decode(encoded_lloydmax)
+        rmse = get_rmse(decoded_lloydmax,cdf_rand_samples)
+        #print np.mean(lloydmax_b)
+        print '  RMSE: '+str(rmse)
+        print '  PSNR: '+str(rmse2psnr(rmse))+' dB'
+        #print '\n'
+    
+lloydmax_learn(0.15)
+print 'success!'
+
+
+
+
+
+
 
 
 
